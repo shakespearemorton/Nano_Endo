505802dafde2c09f0d3b1d8c1b94ebb9e9ec393c
shakespearemorton <45985979+shakespearemorton@users.noreply.github.com>
2022-04-07 17:54:07 +0100

    Update start.py

--- start.py
+++ start.py
@@ -4,14 +4,11 @@ from write import *
 import random
 from tab_pot import *
 
-Timesteps = 10000000
 receptor_f = 0.50
 Box = 60
 Wc = 1.5
-Sigma = 1
-Epsilon = 1
 
 memb, boxx, boxy = hexLipids(Box)
 writeMembrane(memb,receptor_f, boxx, boxy, 20)
 
-tabs(Wc,Sigma,Epsilon)
+tabs(Wc)
